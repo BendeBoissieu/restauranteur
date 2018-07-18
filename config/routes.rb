f801e1be780222a985520139f13aa93e9331eb25
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-07-18 23:39:00 +0200

    almost ready need to check the paths

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ end
 
 
 
-=beginRails.application.routes.draw do
-  get 'static_pages/index'
-  resources :restaurants
+# Rails.application.routes.draw do
+#  get 'static_pages/index'
+#  resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#index'
-end
-=end
+#  root 'static_pages#index'
+# end
