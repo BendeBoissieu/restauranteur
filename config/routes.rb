3b900db4307ac92196285c149c79949982429792
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-07-18 23:16:41 +0200

    download angularjs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
-Rails.application.routes.draw do
+Restauranteur::Application.routes.draw do
+  # Add the following line
+  root 'static_pages#index'
+end
+
+
+
+=beginRails.application.routes.draw do
+  get 'static_pages/index'
   resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#index'
 end
+=end
